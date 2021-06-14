33232d84a9093ab2d40ae2dafa5cec61d5676250
Keifer Stone <keiferstone@gmail.com>
2021-06-14 11:49:23 -0600

    update gist link

--- data/src/main/java/com/teamsnap/data/TeamSnapClient.kt
+++ data/src/main/java/com/teamsnap/data/TeamSnapClient.kt
@@ -7,7 +7,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 val teamSnapClient: TeamSnapApi = Retrofit.Builder()
-    .baseUrl("https://gist.githubusercontent.com/keiferstone/1320bd015645f83e11c28a9e66cf9ae6/raw/32afa4ce7f88cd7fff929b7714f2636305b597c1/")
+    .baseUrl("https://gist.githubusercontent.com/keiferstone/1320bd015645f83e11c28a9e66cf9ae6/raw/78d9035fa4732f0fab041d4c1a512c21a5773947/")
     .addConverterFactory(GsonConverterFactory.create(
         GsonBuilder()
             // TODO Register necessary type adapter(s) here
